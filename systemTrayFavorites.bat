e2ae6c82850f0d9ee9759fabb36b21337c884cbe
Stéphane Mairesse <smairesse@users.noreply.github.com>
2024-02-19 21:58:53 +0100

    Update systemTrayFavorites.bat

--- systemTrayFavorites.bat
+++ systemTrayFavorites.bat
@@ -1,6 +1,6 @@
 @chcp 1250
 
-@rem Powershell.exe -executionpolicy remotesigned -windowstyle hidden -File "%~dp0\systemTrayFavorites.ps1"
+@Powershell.exe -executionpolicy remotesigned -windowstyle hidden -File "%~dp0\systemTrayFavorites.ps1"
 
 @rem powershell.exe -executionpolicy remotesigned -File "%~dp0\systemTrayFavorites.ps1" -path <root directory of favorites path>
 @rem Powershell.exe -executionpolicy remotesigned -windowstyle hidden -File "%~dp0\systemTrayFavorites.ps1" -path <root directory of favorites path>
