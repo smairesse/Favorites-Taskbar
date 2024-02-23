08087aabca172c1a358ff4c8efcda6d1756c2a18
Stéphane Mairesse <smairesse@users.noreply.github.com>
2024-02-23 13:00:55 +0100

    Update to favoritesTaskbar

--- systemTrayFavorites.bat
+++ systemTrayFavorites.bat
@@ -1,8 +1,8 @@
 @chcp 1250
 
-@Powershell.exe -executionpolicy remotesigned -windowstyle hidden -File "%~dp0\systemTrayFavorites.ps1"
+@Powershell.exe -executionpolicy remotesigned -windowstyle hidden -File "%~dp0\favoritesTaskbar.ps1"
 
-@rem powershell.exe -executionpolicy remotesigned -File "%~dp0\systemTrayFavorites.ps1" -path <root directory of favorites path>
-@rem Powershell.exe -executionpolicy remotesigned -windowstyle hidden -File "%~dp0\systemTrayFavorites.ps1" -path <root directory of favorites path>
+@rem powershell.exe -executionpolicy remotesigned -File "%~dp0\favoritesTaskbar.ps1" -path <root directory of favorites path>
+@rem Powershell.exe -executionpolicy remotesigned -windowstyle hidden -File "%~dp0\favoritesTaskbar.ps1" -path <root directory of favorites path>
 
 @rem pause
