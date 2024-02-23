caf42450245188355d1b831dc3122f42d3f6de30
Stéphane Mairesse <smairesse@users.noreply.github.com>
2024-02-23 14:41:50 +0100

    Update favoritesTaskbar.bat

--- favoritesTaskbar.bat
+++ favoritesTaskbar.bat
@@ -1,6 +1,8 @@
 @chcp 1250
 
-@Powershell.exe -executionpolicy remotesigned -windowstyle hidden -File "%~dp0\favoritesTaskbar.ps1"
+@Powershell.exe -executionpolicy remotesigned -File "%~dp0\favoritesTaskbar.ps1"
+
+@if %ERRORLEVEL% neq 0 @pause
 
 @rem powershell.exe -executionpolicy remotesigned -File "%~dp0\favoritesTaskbar.ps1" -path <root directory of favorites path>
 @rem Powershell.exe -executionpolicy remotesigned -windowstyle hidden -File "%~dp0\favoritesTaskbar.ps1" -path <root directory of favorites path>
